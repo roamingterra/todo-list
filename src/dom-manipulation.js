1b073683c618a407d4457dda85010bd765e3ee07
roamingterra <danielballerini1@gmail.com>
2023-02-11 14:51:00 -0500

    Set up project and add initial pseudo-code
    I set up the project files, npm, and webpack. I downloaded a package
    from npm called date-fns which was suggested for its handy functions for
    formatting and manipulating dates and times. Finally, I added pseudo-
    code to the logic.js and dom-manipulation.js files to outline the
    various function. The logic.js file will contain all of the application
    logic, whereas the dom-manipulation.js file will contain all the
    functions that work with the dom tree and load the necessary pages.

---
+++ src/dom-manipulation.js
@@ -0,0 +1,7 @@
+// dashboard function (for loading the side and top bars)
+
+// today function (for loading the today tab which contains all tasks that are due today)
+
+// late function (for loading the late tab which contains all tasks that were due before the present date and have not been completed yet)
+
+// taskCategory function (for loading the content of a taskCategory that has been selected)
---
+++ src/logic.js
@@ -0,0 +1,28 @@
+// taskCategory factory function (creates task category objects, which will contain tasks)
+// properties: title (received from user form), color (received from user form), array of tasks
+// methods: getTitle, getColor, setTitle, setColor
+
+// task factory function (creates tasks that the user creates, which will be placed in a taskCategory)
+// properties: title (received from user form), description (received from user form),
+//             category (received from user form), dueDate (received from user form),
+//             priority (received from user form)
+// methods: getTitle, getDescription, getCategory, getDueDate, getPriority
+//          setTitle, setDescription, setCategory, setDueDate, setPriority
+
+// today array (contains all task objects that have dueDate of todays date)
+
+// late array (contains all task objects that have a dueDate earlier that todays date)
+
+// setComplete function (sets task as complete and deletes it from all arrays that contain it)
+// This function performs an animation before deleting it unlike the next function...
+
+// deleteTask function (deletes task from all arrays that contains it)
+
+// addTask function (receives data from the user form and calls the task factory function, feeding it the user parameters)
+// ***This may not be needed***
+
+// organizeTask function (sends task object to appropriate arrays)
+
+// editTask function (receives data from user form, then calls task set methods to change the appropriate properties)
+
+// changeTheme function (changes UI to dark mode or light mode )
